b7f5c1d7adcefed022346103ff07793c21153fa5
Honghee Choi <vvshinevv@nhnsoft.com>
2021-12-04 04:51:59 +0900

    1차 작업 내용 진행

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PromotionLayoutComponent } from './pages/promotion/layout/promotion-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: PromotionLayoutComponent,
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: false })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
